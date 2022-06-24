b8a7f43fad06ba09ae357200f2b3f4836ea14020
Yukesh <yukesh@MacBook-Pro.local>
2022-06-24 07:35:04 +0545

    3 contane

---
+++ project/lib/login.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                child: Text(
+                  'data',
+                  style: TextStyle(
+                    color: Colors.amber,
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text('data'),
+              ),
+              Container(
+                child: Text('data'),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
